ae4f8a85f0419ef104e23ebd5973ab8579274919
Patrick Oldman <patrick@ecce.uk>
2022-11-04 13:10:23 +0000

    Add photo frame to saved image

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,14 @@
 // Set constraints for the video stream
-var constraints = { video: { facingMode: "user" }, audio: false };
+let constraints = { video: { facingMode: "user" }, audio: false };
 // Define constants
 const cameraView = document.querySelector("#camera--view"),
   cameraOutput = document.querySelector("#camera--output"),
-  cameraOutputWrapper = document.querySelector("#camera--output-wrapper");
+  cameraOutputWrapper = document.querySelector("#camera--output-wrapper"),
   cameraSensor = document.querySelector("#camera--sensor"),
-  cameraTrigger = document.querySelector("#camera--trigger");
-
+  cameraTrigger = document.querySelector("#camera--trigger"),
+  frame = document.querySelector("#overlay--frame-img"),
   saveButton = document.querySelector("#camera--output-save");
+
 // Access the device camera and stream to cameraView
 function cameraStart() {
   navigator.mediaDevices
@@ -20,18 +21,23 @@ function cameraStart() {
       console.error("Oops. Something is broken.", error);
     });
 }
+
 // Take a picture when cameraTrigger is tapped
 cameraTrigger.onclick = function () {
   cameraSensor.width = cameraView.videoWidth;
   cameraSensor.height = cameraView.videoHeight;
-  cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+  let context = cameraSensor.getContext("2d");
+  context.drawImage(cameraView, 0, 0);
+  context.drawImage(frame, 0, 0, cameraView.videoWidth, cameraView.videoHeight);
   cameraOutput.src = cameraSensor.toDataURL("image/webp");
   cameraOutputWrapper.classList.add("taken");
+  cameraOutputWrapper.style.width = cameraView.videoWidth * 0.8;
+  cameraOutputWrapper.style.height = cameraView.videoHeight * 0.8;
   saveButton.style.display = "block";
 };
-// add image source to download output
+// Add image source to download output
 saveButton.onclick = function () {
-    saveButton.href = cameraOutput.src;
-}
+  saveButton.href = cameraOutput.src;
+};
 // Start the video stream when the window loads
 window.addEventListener("load", cameraStart, false);
