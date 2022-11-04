0d0198d90008a4342501b7b86678ae5ffa80513d
Patrick Oldman <patrick@ecce.uk>
2022-11-04 11:08:44 +0000

    enable saving image and add minor styling to buttons

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ var constraints = { video: { facingMode: "user" }, audio: false };
 // Define constants
 const cameraView = document.querySelector("#camera--view"),
   cameraOutput = document.querySelector("#camera--output"),
+  cameraOutputWrapper = document.querySelector("#camera--output-wrapper");
   cameraSensor = document.querySelector("#camera--sensor"),
   cameraTrigger = document.querySelector("#camera--trigger");
+
+  saveButton = document.querySelector("#camera--output-save");
 // Access the device camera and stream to cameraView
 function cameraStart() {
   navigator.mediaDevices
@@ -23,7 +26,12 @@ cameraTrigger.onclick = function () {
   cameraSensor.height = cameraView.videoHeight;
   cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
   cameraOutput.src = cameraSensor.toDataURL("image/webp");
-  cameraOutput.classList.add("taken");
+  cameraOutputWrapper.classList.add("taken");
+  saveButton.style.display = "block";
 };
+// add image source to download output
+saveButton.onclick = function () {
+    saveButton.href = cameraOutput.src;
+}
 // Start the video stream when the window loads
 window.addEventListener("load", cameraStart, false);
