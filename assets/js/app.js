d4fb7adb32f7629993069e703728eb9850769cfa
Patrick Oldman <patrick@ecce.uk>
2022-11-25 14:45:39 +0000

    Add image editing

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,17 +5,43 @@ const cameraView = document.querySelector("#camera--view"),
   cameraOutput = document.querySelector("#camera--output"),
   cameraOutputWrapper = document.querySelector("#camera--output-wrapper"),
   cameraSensor = document.querySelector("#camera--sensor"),
+  context = cameraSensor.getContext("2d"),
   cameraTrigger = document.querySelector("#camera--trigger"),
   frame = document.querySelector("#overlay--frame-img"),
-  saveButton = document.querySelector("#camera--output-save");
+  saveButton = document.querySelector("#camera--output-save"),
+  frameSelectors = document.querySelectorAll(".frame--selector"),
+  textInput = document.querySelector("#overlay--text-input"),
+  text = document.querySelector("#overlay--text-content"),
+  editImage = document.querySelector("#edit-image"),
 
-  frameSelectors = document.querySelectorAll(".frame--selector");
+  editedCanvasWrapper = document.querySelector("#edited--canvas--wrapper"),
 
-  textInput = document.querySelector("#overlay--text-input");
 
-  text = document.querySelector("#overlay--text-content");
+  // Get the canvas for the edited image
+  editedCanvas = editedCanvasWrapper.querySelector("#editedCanvas"),
+  // Get the 2D context of the image
+  editedContext = editedCanvas.getContext("2d"),
 
-  hiddens = document.querySelectorAll(".hidden");
+  hiddens = document.querySelectorAll(".hidden"),
+
+  // Get the sliders
+  brightnessSlider = document.querySelector("#brightnessSlider"),
+  contrastSlider = document.querySelector("#contrastSlider"),
+  grayscaleSlider = document.querySelector("#grayscaleSlider"),
+  hueRotateSlider = document.querySelector("#hueRotateSlider"),
+  saturateSlider = document.querySelector("#saturationSlider"),
+  sepiaSlider = document.querySelector("#sepiaSlider")
+  ;
+
+// Start the video stream when the window loads
+window.addEventListener("load", cameraStart, false);
+frameSelectors.forEach((selector) => {
+  selector.onclick = function () {
+    frame.style.display = "block";
+    let src = selector.dataset.img;
+    frame.src = "/assets/img/" + src;
+  };
+});
 
 // Access the device camera and stream to cameraView
 function cameraStart() {
@@ -24,9 +50,11 @@ function cameraStart() {
     .then(function (stream) {
       track = stream.getTracks()[0];
       cameraView.srcObject = stream;
-      hiddens.forEach(hidden => {
+
+      // Remove all hidden items on page load so they dont obstruct loading screen
+      hiddens.forEach((hidden) => {
         hidden.classList.remove("hidden");
-      })
+      });
     })
     .catch(function (error) {
       console.error("Oops. Something is broken.", error);
@@ -35,37 +63,131 @@ function cameraStart() {
 
 // Take a picture when cameraTrigger is tapped
 cameraTrigger.onclick = function () {
-  cameraSensor.width = cameraView.videoWidth;
-  cameraSensor.height = cameraView.videoHeight;
-  let context = cameraSensor.getContext("2d");
-  context.drawImage(cameraView, 0, 0);
-  context.drawImage(frame, 0, 0, cameraView.videoWidth, cameraView.videoHeight);
+  let width = cameraView.videoWidth;
+  let height = cameraView.videoHeight;
+  cameraSensor.width = width;
+  cameraSensor.height = height;
 
+  context.drawImage(cameraView, 0, 0);
+  context.drawImage(frame, 0, 0, width, height);
+
+  // Add text, I dont want comic sans but i cant figure out how to retain the text from the dom
   context.font = "26px Comic Sans MS";
   context.fillStyle = "red";
   context.textAlign = "center";
-  context.fillText(text.innerHTML, cameraView.videoWidth / 2, cameraView.videoHeight * 0.4);
-  cameraOutput.src = cameraSensor.toDataURL("image/webp");
+  context.fillText(text.innerHTML, width / 2, height * 0.4);
+
+  // Add src to img using the camerasensor data
+  let img = cameraSensor.toDataURL("image/png");
+  cameraOutput.src = img;
   cameraOutputWrapper.classList.add("taken");
-  cameraOutputWrapper.style.width = cameraView.videoWidth;
-  cameraOutputWrapper.style.height = cameraView.videoHeight;
-  saveButton.style.display = "block";
+  cameraOutputWrapper.style.width = width;
+  cameraOutputWrapper.style.height = height;
+
+  editedCanvas.width = width;
+  editedCanvas.height = height;
+
+  cameraOutputWrapper.style.display = "block";
 };
-// Add image source to download output
-saveButton.onclick = function () {
-  saveButton.href = cameraOutput.src;
-};
-// Start the video stream when the window loads
-window.addEventListener("load", cameraStart, false);
-frameSelectors.forEach(selector => {
-  selector.onclick = function () {
-    let src = selector.dataset.img;
-    frame.src = "/assets/img/" + src;
-  }
-});
 
 textInput.onchange = function () {
   if (textInput.value) {
     text.innerHTML = "Merry Christmas from " + textInput.value;
   }
+};
+
+// Reset all the slider values to their default values
+function resetImage() {
+  brightnessSlider.value = 100;
+  contrastSlider.value = 100;
+  grayscaleSlider.value = 0;
+  hueRotateSlider.value = 0;
+  saturateSlider.value = 100;
+  sepiaSlider.value = 0;
+  applyFilter();
+}
+
+function saveImage() {
+  // Select the temporary element we have created for
+  // helping to save the image
+  saveButton.setAttribute("download", "christmas.png");
+
+  // Convert the canvas data to a image data URL
+  let canvasData = editedCanvas.toDataURL("image/png");
+
+  // Replace it with a stream so that
+  // it starts downloading
+  canvasData.replace("image/png", "image/octet-stream");
+
+  // Set the location href to the canvas data
+  saveButton.setAttribute("href", canvasData);
+}
+
+// This function is used to update the image
+// along with all the filter values
+function applyFilter() {
+  // Create a string that will contain all the filters
+  // to be used for the image
+  let filterString =
+    "brightness(" +
+    brightnessSlider.value +
+    "%" +
+    ") contrast(" +
+    contrastSlider.value +
+    "%" +
+    ") grayscale(" +
+    grayscaleSlider.value +
+    "%" +
+    ") saturate(" +
+    saturateSlider.value +
+    "%" +
+    ") sepia(" +
+    sepiaSlider.value +
+    "%" +
+    ") hue-rotate(" +
+    hueRotateSlider.value +
+    "deg" +
+    ")";
+
+  // Apply the filter to the image
+  editedContext.filter = filterString;
+
+  // Draw the edited image to canvas
+  editedContext.drawImage(cameraOutput, 0, 0);
+}
+
+// Each of these will first reset the image
+// and then apply a certain parameter before
+// redrawing the image
+function brightenFilter() {
+  resetImage();
+  brightnessSlider.value = 130;
+  contrastSlider.value = 120;
+  saturateSlider.value = 120;
+  applyFilter();
+}
+
+function bwFilter() {
+  resetImage();
+  grayscaleSlider.value = 100;
+  brightnessSlider.value = 120;
+  contrastSlider.value = 120;
+  applyFilter();
+}
+
+function funkyFilter() {
+  resetImage();
+
+  // Set a random hue rotation everytime
+  hueRotateSlider.value = Math.floor(Math.random() * 360) + 1;
+  contrastSlider.value = 120;
+  applyFilter();
+}
+
+function vintageFilter() {
+  resetImage();
+  brightnessSlider.value = 120;
+  saturateSlider.value = 120;
+  sepiaSlider.value = 150;
+  applyFilter();
 }
